0a612ae559d027e944006494ed34032d8ca9c1c7
mhdadnan05 <mohammedadnan93901@gmail.com>
2025-05-21 20:12:31 +0530

    Add files via upload

---
+++ CollegeDetails.java
@@ -0,0 +1,8 @@
+public class CollegeDetails{
+		public static void main (String[]args){
+			System.out.println("CollegeName: Kakatiya Institute of Technology and Science");
+			System.out.println("Course: B Tech in Information Technology");
+			System.out.println("Year: pursued in 2025");
+			System.out.println("Location: Warangal");
+ 	}
+}
